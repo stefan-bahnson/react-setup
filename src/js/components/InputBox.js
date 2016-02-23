4a102133cbd40e523a1ebbb430b479b23aa3134b
Stefan Bahnson <stefanbahnson@gmail.com>
2016-02-23 13:02:07 +0100

    create InputBox comp

---
+++ src/js/components/InputBox.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class InputBox extends React.Component {
+    render() {
+        const { handleClick } = this.props;
+        console.log(handleClick);
+        return (
+            <div className="InputBox">
+                <input
+                    ref={ text => this._input = text}
+                    type="text"
+                    placeholder="Send a message"/>
+                <i
+                    className="material-icons"
+                    onClick={() => {
+                        handleClick(this._input.value);
+                        this._input.value = null;
+                    }}>
+                    send
+                </i>
+            </div>
+        );
+    }
+}
+export default InputBox

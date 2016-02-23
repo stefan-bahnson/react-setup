6c7e1fb47a2bac3a792a4e6e7ed26fcef0353b0a
Stefan Bahnson <stefanbahnson@gmail.com>
2016-02-23 13:04:45 +0100

    refactor app

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,19 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+import Inbox from './Inbox'
+
+require('./_app.sass');
+
 class App extends React.Component {
 
-  render() {
-      return (
-          <div>
-              <h1>React Setup</h1>
-              <Link to='/'>Home</Link>
-              <Link to='/inbox'>Inbox</Link>
-              {this.props.children}
-          </div>
-      )
-  }
+    render() {
+        const {messages} = this.props;
+        return (
+            <div>
+                <Header />
+                <div className="Mail">
+                    <Compose>
+                        {this.props.children}
+                    </Compose>
+                    <Inbox />
+                </div>
+            </div>
+        )
+    }
 
 }
 
 export default App
+
+const Header = () => (
+    <div className="Header">
+        <h1>React Setup</h1>
+        <Link to="/">InstaMail</Link>
+        <Link to="/snailmail">SnailMail</Link>
+    </div>
+);
+
+const Compose = (props) => (
+    <div className="Compose">
+        {props.children}
+    </div>
+);

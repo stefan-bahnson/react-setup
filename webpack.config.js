2c6e8b1fc5e15e464c51f77d1ffe095dcb92fe60
Stefan Bahnson <stefanbahnson@gmail.com>
2016-02-23 13:05:39 +0100

    update webconfig for sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+
+var sassLoaders = [
+    "css-loader?sourceMap",
+    "sass-loader?indentedSyntax=sass&includePaths[]=" + path.resolve(__dirname, "./src/stylesheets")
+];
+
 module.exports = {
     entry: "./src/js/main",
     output: {
@@ -19,7 +27,14 @@ module.exports = {
                 query: {
                     presets: ['es2015', 'react', 'stage-2']
                 }
+            },
+            {
+                test: /\.sass$/,
+                loader: ExtractTextPlugin.extract("style-loader", sassLoaders.join("!"))
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ]
 };

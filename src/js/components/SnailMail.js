43b8d4a614b2d2375ff511a6d850fe0d79780414
Stefan Bahnson <stefanbahnson@gmail.com>
2016-02-23 13:02:50 +0100

    create SnailMail comp

---
+++ src/js/components/SnailMail.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { setStateAsync } from '../actions/message.actions'
+
+import InputBox from './InputBox'
+
+class SnailMail extends React.Component {
+    render() {
+        const { dispatch } = this.props;
+        return (
+            <div className="SnailMail">
+                <h1>SnailMail</h1>
+                <InputBox handleClick={( msg => dispatch(setStateAsync(msg)) )}/>
+            </div>
+        );
+    }
+}
+
+export default connect()(SnailMail)

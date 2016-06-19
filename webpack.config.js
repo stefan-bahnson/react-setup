3f6e6ebf6c7f6aca338a286a542e808a2192cb2b
Eckrin <stefanbahnson@gmail.com>
2016-06-19 17:17:52 +0200

    fix local webpack-dev-server and resolve url for files in sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,39 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 
 module.exports = {
-    entry: "./src/js/main",
-    output: {
-        path: './dist',
-        filename: "bundle.js",
-        publicPath: '/'
-    },
-    devServer: {
-        inline: true,
-        port: 3000,
-        contentBase: './dist'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react', 'stage-0', 'stage-2']
-                }
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract("style", "css!sass")
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('styles.css')
+  entry: './src/js/main',
+  output: {
+    path: './dist',
+    filename: "bundle.js",
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    port: 3000,
+    contentBase: './dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-0', 'stage-2']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass', 'resolve-url')
+      },
+      {
+        test: /\.(eot|ttf|svg|gif|png|jpg|jpeg)$/,
+        loader: 'file-loader'
+      }
     ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
 };
